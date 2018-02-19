b35b80ac823d1e11f84f95bdca7a09e79b48f6b2
VladPerish <perish.abs@gmail.com>
2018-02-19 08:52:23 +0600

    Corrected tasks 2 and 4

--- librarian.js
+++ librarian.js
@@ -3,8 +3,9 @@ var library = [
   { title: "The Old Man and the Sea", year: "1952", author: "Ernest Hemingway", publisher: "Charles Scribner's Sons" },
   { title: "Mythos: A Retelling of the Myths of Ancient Greece", year: "1981", author: "Stephen Fry", publisher: "Penguin" },
   { title: "The Mythical Man-Month", year: "1975", author: "Fred Brooks", publisher: "Addison-Wesley" },
-  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" }
-  /* Task 2 */
+  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" },
+  { title: "A SOng of Ice and Fire", Year: "1996", author: "G.R.R Martin", publisher: "Bantam Books" },
+
 ];
 
 /* Task 1 */
@@ -50,14 +51,19 @@ console.log(bookByAuthor());
 /* Task 5 */
 function findByTitle( title ){
 
+  var matches= [];
+
   var pat= new RegExp(title);
   //add code
   for (var i = 0; i < library.length; i++) {
     if (library[i].title.match(pat)) {
 
-      return library[i].title;
+      matches[i]=library[i].title;
 
     }
+  }
+  for (var i = 0; i < matches.length; i++) {
+     console.log(matches[i]);
   }
 }
 
